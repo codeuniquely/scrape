1890a1b79899b352a57530bc4451b1ffc1fec565
Steve Saxton <steves@codeuniquely.co.uk>
2018-04-13 13:17:27 +0100

    Code Update

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const config = {
   urlColumn: 2,
   imageColumn: 'image',
   savePath: 'images',
+  trimTail: '/revision/latest',
   site: 'http://marvel.wikia.com/wiki/',
   target: '#mw-content-text figure.pi-image a.image-thumbnail',
   index: 0,
--- index.js
+++ index.js
@@ -8,79 +8,80 @@ var url = require("url");
 var cheerio = require("cheerio");
 var csv = require('fast-csv');
 
-
-// let webRoot = 'http://marvel.wikia.com/wiki/';
 const config = require('./config.js');
 
-// const scrape = new Scraper();
 const savePath = path.resolve(__dirname, config.savePath);
 
 // processed record count
 let records = 0;
-let currentImage = '';
-let writeFilename = '';
 
 // Processing data
 let header = [];
 let processedRecords = [];
 
-// // The "Image" class.
-// function Image(image, address){
-//   var at = this.attributes = image.attribs;
-//   this.name = path.basename(at.src, path.extname(at.src));
-//   this.saveTo = path.dirname(require.main.filename) + "/";
-//   this.extension = path.extname(at.src);
-//   this.address = url.resolve(address, at.src);
-//   this.fromAddress = address;
-// }
-
-// the image
-let image = null;
-
+// Functions
+// ===================
 function clearArray() {
   processedRecords = [];
 }
 
-function save(callback){
+function ensureWritePath() {
+  const writePath = path.resolve(__dirname, config.savePath);
+  if (!fs.existsSync(writePath)) {
+    fs.mkdirSync(writePath);
+  }
+}
 
-  var parsedUrl = url.parse(this.address);
+function getImage(id, imageUrl, callback){
+  var parsedUrl = url.parse(imageUrl);
 
   // Support HTTPS.
-  var protocol = http;
+  let protocol = http;
   if(parsedUrl.protocol === "https:") {
     protocol = https;
   }
 
-  // Make a reference to the current instance.
-  // var ref = this;
+  // get the file type - Just SMASH it all together - no checks
+  const noquery = parsedUrl.href.replace(`?${parsedUrl.query}`, '');
+  const download = noquery.replace(config.trimTail, '');
+  const postdot = download.split('.').pop(); // .png
+  const ext = postdot.split('?')[0].toLowerCase();
 
-  var request = protocol.request(this.address, function(response){
+  // build the writeable filename
+  const filename = `image${id}.${ext}`;
+
+  // Make a reference to the current instance.
+  const savedPath = path.resolve(__dirname, config.savePath, filename);
+
+  // WHAT IS THE IMAGE
+  // const reqImageUrl = '' + `${download}`;
+  // console.log('Getting image', download.replace(config.site, '...'));
+
+  var request = protocol.request(download, response => {
     if(response.statusCode != 200){
-      console.error(`Image scraper(3): image couldn't be found. (statusCode: + ${response.statusCode})`);
-      return request.end();
-    } else{
-      var imageFile = fs.createWriteStream(path.resolve(savePath, ref.name + ref.extension));
+      console.error(`scrape(3): Error Image, (statusCode: + ${response.statusCode}), ${download}`);
+      // return request.end();
+    } else {
+      var imageFile = fs.createWriteStream(savedPath);
       imageFile.on('error', function(e){
-        console.error(`Image scraper(4): error while loading image: ${e}.`);
+        console.error(`scrape(4): error while loading image: ${e}.`);
       });
-      response.on('data', function(data) {
+      response.on('data', data => {
         imageFile.write(data);
       });
-      response.on('end', function(){
+      response.on('end', () => {
         imageFile.end();
-        if (typeof(callback) === 'function') {
-          callback.call(ref);
-        }
+        callback(filename);
       });
     }
   });
   request.end();
-  request.on('error', function(e) {
-    console.error(e);
+  request.on('error', e => {
+    console.error(`scrape(5): Error while loading image: ${e}.`);
   });
 };
 
-function scrape(website, id, callback) {
+function scrape(id, website, callback) {
   const parsedUrl = url.parse(website);
 
   // Support HTTPS.
@@ -89,33 +90,46 @@ function scrape(website, id, callback) {
     protocol = https;
   }
 
-  var request = protocol.request(website, function(response) {
+  let request = protocol.request(website, response => {
     if(response.statusCode === 200){
       response.setEncoding('utf8');
 
-      var current;
-      var previous = '';
+      let page = '';
+      response.on('data', data => {
+        page += data;
+      });
 
-      response.on('data', function(data) {
-        var current = previous + data;
-        current.replace(/<img[\S\s]*?>/ig, function(m) {
-          var image = new Image(cheerio.load(m)('img')[0], website);
-          callback(id, image);
-        });
-        previous = data;
+      // Once the wholepage is loaded
+      response.on('end', function() {
+        // stick the page into a cheerio DOM doc
+        const $ = cheerio.load(page);
+
+        // Target the Image DOM node required
+        const target = $(`${config.target}`);
+        if (target.length > 0) {
+          const imageUrl = target[0].attribs.href;
+          if (imageUrl) {
+            getImage(id, imageUrl, callback);
+          } else {
+            console.log('No HREF for target', target[0]);
+            callback('missing.jpg');
+          }
+        } else {
+          console.log('No IMAGE for', website);
+          callback('missing.jpg');
+        }
       });
     } else {
-      console.error(`Image scraper(1): web page couldn't be found. (statusCode: ${response.statusCode})`);
-      request.end();
-      return process.exit(1);
+      console.error(`scrape(1): Error (statusCode: ${response.statusCode}), ${website}`);
+      callback('missing.jpg');
     }
   });
 
-  request.end();
-
   request.on('error', function(e){
-    console.error(`Image scraper(2): error while loading web page: ${e}.`);
+    console.error(`scrape(2): Error while loading web page: ${e}.`);
   });
+
+  request.end();
 };
 
 function buildRecord(data) {
@@ -139,23 +153,22 @@ function processRecord(data) {
   if (records === 0) {
     header = data;
   } else {
-    console.log('.');
     processedRecords.push(buildRecord(data));
   }
   records++;
 }
 
 function downloadImages(done) {
-  console.log('START DOWNLOAD');
+  console.log('Starting download...');
   function processRecord(index) {
     if (index === processedRecords.length) {
-      console.log('END DOWNLOAD');
-      // write the new CSV to disc
-      done();
+      console.log('Completed download.');
+      return done();
     }
     const record = processedRecords[index];
-    scrape(record.webpage, record.id, function(filename) {
-      processedRecords.filename = filename;
+    const id = record[config.idColumn];
+    scrape(id, record.webpage, filename => {
+      processedRecords[index].filename = filename;
       processRecord(index + 1);
     });
   }
@@ -164,21 +177,37 @@ function downloadImages(done) {
 
 // loop through all teh record and write them to a csv
 function saveNewCSV() {
+  const filename = path.resolve(__dirname, config.savePath, 'processed.csv');
+  const csvStream = csv.format({ headers: true });
+  const writableStream = fs.createWriteStream(filename);
+
+  writableStream.on('finish', () => {
+    console.log(`processed ${records} records`);
+    console.log('Finished!'); // eslint-disable-line no-console
+  });
+
+  csvStream.pipe(writableStream);
+  for(let i = 0; i < processedRecords.length; i++) {
+    const record = processedRecords[i];
+    csvStream.write(record);
+  }
+
+  csvStream.end();
 }
 
 function run () {
   // clear down the array
   clearArray();
+  ensureWritePath();
 
   csv.fromPath(config.file)
-    .on('data', function(data){
+    .on('data', data => {
       processRecord(data);
     })
     .on('end', function(){
       console.log('CSV ENDS');
-      downloadImages(function() {
+      downloadImages(() => {
         saveNewCSV();
-        console.log(`finished procesing ${records}`);
       });
     });
 }
